ab626ba1cc2ffbbc378326420c7fd9cacf583830
Nguyễn Việt Phương <51957734+vietphuong452@users.noreply.github.com>
2019-09-28 20:48:43 +0700

    Add files via upload

--- 9.py
+++ 9.py
@@ -2,14 +2,16 @@ import math
 a=int(input("So thu nhat: "))
 b=int(input("So thu hai: "))
 c=int(input("So thu ba: "))
-denta= b*b-4*a*c
-if denta<0:
-    print("Vo nghiem")
-elif denta>0:
-    print("Co 2 nghiem phan biet: ")
-    e= ((-b) + (math.sqrt(denta)))/(2*a)
-    g= ((-b) - (math.sqrt(denta)))/(2*a)
-    print("X2 : ",g,"X1 : ",e)
-else:
-    f=-b/(2*a)
-    print("x1=x2=",f)
+def quadro(a,b,c):
+    denta= b*b-4*a*c
+    if denta<0:
+        print("Vo nghiem")
+    elif denta>0:
+        print("Co 2 nghiem phan biet: ")
+        e= ((-b) + (math.sqrt(denta)))/(2*a)
+        g= ((-b) - (math.sqrt(denta)))/(2*a)
+        print("X2 : ",g,"X1 : ",e)
+    else:
+        f=-b/(2*a)
+        print("x1=x2=",f)
+quadro(a,b,c)
